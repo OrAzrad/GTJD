7e049600f6153e6e99f78f28c00e5aeb6743076b
Or Azrad <orazrad2001@gmail.com>
2019-03-28 11:07:13 +0200

    starting commit

---
+++ app/src/main/java/com/example/gtjd/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.gtjd;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.content.Intent;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+               // Toast.makeText(MainActivity.this, "Or KATAV",Toast.LENGTH_LONG).show();
+                //
+                Intent intent = new Intent(MainActivity.this, registerScreen.class);
+                startActivity(intent);
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/gtjd/registerScreen.java
@@ -0,0 +1,19 @@
+package com.example.gtjd;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class registerScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_screen);
+
+    }
+
+}
