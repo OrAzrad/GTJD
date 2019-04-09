955fdca3efb460fe4de87f491bf63fc463e975a9
Or Azrad <orazrad2001@gmail.com>
2019-04-09 19:14:59 +0300

    starting commit

--- app/src/main/java/com/example/gtjd/MainActivity.java
+++ app/src/main/java/com/example/gtjd/MainActivity.java
@@ -1,20 +1,88 @@
 package com.example.gtjd;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Patterns;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 import android.content.Intent;
 
-
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 
 public class MainActivity extends AppCompatActivity implements  View.OnClickListener {
+
+    FirebaseAuth mAuth;
+    EditText email,password;
+    ProgressBar progress_bar;
+
+    public String GetString(EditText str){
+
+        String str_to_return = "";
+        str_to_return = str.getText().toString();
+        return str_to_return;
+    }
+
+    private void Login(EditText email, EditText password)
+    {
+
+        String email_str = GetString(email);
+        String password_str = GetString(password);
+
+        if(email_str.length() == 0)
+        {
+            email.setError("Email is requierd");
+            email.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email_str).matches())
+        {
+            email.setError("Enter a valid Email");
+            email.requestFocus();
+            return;
+        }
+        if (password_str.length() < 7){
+            password.setError("Password must contain at least 7 characters");
+            password.requestFocus();
+            return;
+        }
+
+        progress_bar.setVisibility(View.VISIBLE);
+        mAuth.signInWithEmailAndPassword(email_str, password_str).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful())
+                {
+                    progress_bar.setVisibility(View.GONE);
+                    Intent intent = new Intent(MainActivity.this, CreateProfile.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }
+                else
+                {
+                    progress_bar.setVisibility(View.GONE);
+                    Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +90,15 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
         setContentView(R.layout.activity_main);
 
         findViewById(R.id.textViewSignUp).setOnClickListener(this);
+        findViewById(R.id.buttonLogin).setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        email = findViewById(R.id.editTextEmail);
+        password = findViewById(R.id.editTextPassword);
+        progress_bar = findViewById(R.id.progress_bar);
+
+
 
     }
 
@@ -29,8 +106,11 @@ public class MainActivity extends AppCompatActivity implements  View.OnClickList
     public void onClick(View v) {
         switch(v.getId()){
             case R.id.textViewSignUp:
-
                 startActivity(new Intent(this, registerScreen.class));
+                break;
+
+            case R.id.buttonLogin:
+                Login(email, password);
                 break;
         }
 
--- app/src/main/java/com/example/gtjd/registerScreen.java
+++ app/src/main/java/com/example/gtjd/registerScreen.java
@@ -1,16 +1,26 @@
 package com.example.gtjd;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 
 
 public class registerScreen extends AppCompatActivity {
 
-    private EditText password;
-    private EditText confirm_password;
+    private EditText user_name, email, password, confirm_password;
+    ProgressBar progress_bar;
+    private FirebaseAuth mAuth;
 
 
     public String GetString(EditText str){
@@ -20,21 +30,60 @@ public class registerScreen extends AppCompatActivity {
         return str_to_return;
     }
 
-    public void validation(EditText password, EditText confirm_password) {
-
-       String password_str =   GetString(password);
-       String confirm_password_str =   GetString(confirm_password);
+    public void validation(EditText user_name, EditText email, EditText password, EditText confirm_password, final ProgressBar progress_bar) {
 
 
+
+        String user_name_str = GetString(user_name);
+        String email_str = GetString(email);
+        String password_str =   GetString(password);
+        String confirm_password_str =   GetString(confirm_password);
+
+
+        if(email_str.length() == 0)
+        {
+            email.setError("Email is requierd");
+            email.requestFocus();
+            return;
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(email_str).matches())
+        {
+            email.setError("Enter a valid Email");
+            email.requestFocus();
+            return;
+        }
 
         if (password_str.length() < 7){
             password.setError("Password must contain at least 7 characters");
+            password.requestFocus();
+            return;
         }
         if (!password_str.equals(confirm_password_str)) {
             confirm_password.setError("Password's are different");
+            return;
         }
 
-
+        progress_bar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email_str, password_str)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            progress_bar.setVisibility(View.GONE);
+                            Toast.makeText(getApplicationContext(), "User registered!", Toast.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            progress_bar.setVisibility(View.GONE);
+                            if (task.getException() instanceof FirebaseAuthUserCollisionException)
+                                Toast.makeText(getApplicationContext(), "Email is already registered", Toast.LENGTH_SHORT).show();
+                            else
+                            {
+                                Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
 
     }
 
@@ -43,14 +92,23 @@ public class registerScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_screen);
 
-         password = findViewById(R.id.Password);
-         confirm_password = findViewById(R.id.Confirm_password);
+
+
+
+
+        user_name = findViewById(R.id.editTextUserName);
+        email = findViewById(R.id.editTextEmail);
+        password = findViewById(R.id.editTextPassword);
+        confirm_password = findViewById(R.id.editTextConfirm_password);
+        progress_bar = findViewById(R.id.progress_bar);
+
+        mAuth = FirebaseAuth.getInstance();
 
         findViewById(R.id.Regisrer).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                validation(password, confirm_password);
+                validation(user_name, email, password, confirm_password, progress_bar);
 
 
             }
